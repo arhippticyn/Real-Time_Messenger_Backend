d077a0641b07eb1c7d3097b23eab3d18e4346e85
arhippticyn <kakashihatake6hokagegygg@gmail.com>
2026-03-23 00:29:33 +0200

    write message models

---
+++ app/models/message/message_status.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, Enum, DateTime
+from ...db.base import Base
+import enum
+from sqlalchemy.sql import func
+
+class MessageStatusType(str, enum.Enum):
+    sent = 'sent'
+    delivered = 'delivered'
+    read = 'read'
+
+
+class MessageStatus(Base):
+    __tablename__ = 'message_status'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    message_id: Mapped[int] = mapped_column(ForeignKey('message.id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    status: Mapped[MessageStatusType] = mapped_column(Enum(MessageStatusType))
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+
+    message: Mapped['Message'] = relationship('Message', foreign_keys=[message_id])
+    user: Mapped['User'] = relationship('User', foreign_keys=[user_id])
--- migrations/env.py
+++ migrations/env.py
@@ -9,7 +9,8 @@ from app.db.base import Base
 from app.models.user.user import User
 from app.models.chat.chat import Chat
 from app.models.chat.chatParticant import ChatParticipant
-
+from app.models.message.message import Message
+from app.models.message.message_status import MessageStatus
 
 config = context.config
 
