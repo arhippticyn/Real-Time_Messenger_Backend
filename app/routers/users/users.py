34283961f1a5d2f6ebe21d9d7af5000f9bced507
arhippticyn <kakashihatake6hokagegygg@gmail.com>
2026-04-06 23:15:28 +0300

    Remake endpoint get users

--- app/routers/users/users.py
+++ app/routers/users/users.py
@@ -10,9 +10,13 @@ from sqlalchemy import select
 router = APIRouter()
 
 @router.get('', response_model=list[UserResponse])
-async def get_users_by_search(search: str,user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
-    users = (await db.execute(select(User).where(User.id != user.id, User.username == search))).scalars().all()
+async def get_users_by_search(search: str | None = None,user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    query = select(User).where(User.id != user.id)
 
+    if search:
+        query = query.where(User.username.ilike(f'%{search}%'))  # частичный поиск
+
+    users = (await db.execute(query)).scalars().all()
     return users
 
 
